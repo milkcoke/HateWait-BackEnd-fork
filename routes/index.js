e92f79e221d1a78b162f3751f62b57af72b55cc8
milkcoke <mbh023@naver.com>
2020-09-17 22:45:31 +0900

    "메인 소개"

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'HateWait', teamNumber: 3, teamMember: ['조예린','손영호','문승훈'] });
 });
 
 module.exports = router;
