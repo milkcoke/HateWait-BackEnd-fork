3cb8a216cf0336f7fb5e0c9201d2d0bde350fc35
milkcoke <mbh023@konkuk.ac.kr>
2020-09-28 04:34:38 +0900

    db 연동 테스트 완료

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const memberRouter = require('./routes/customer');
+const customerRouter = require('./routes/customer');
 
 const app = express();
 
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/member', memberRouter);
+app.use('/customer', customerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
