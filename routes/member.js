949fe763e098f9b6600373fc826447ac1f9c263f
milkcoke <mbh023@konkuk.ac.kr>
2020-09-28 04:26:47 +0900

    "오타 수정"

--- routes/member.js
+++ routes/member.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db.js');
+const db = require('../db/db.js');
 
 router.get('/', function(req, res) {
     db.query('SELECT * FROM member', (error, rows) => {
