85f6a63c6f587bb5c9242ac63f93edcb2695327f
문승훈 <mbh023@gmail.com>
2020-09-28 13:13:58 +0900

    (Resolved) Disconnect Error Handling + ERR_HTTP_HEADERS_SENT

--- routes/customer.js
+++ routes/customer.js
@@ -4,8 +4,12 @@ const dbConnection = require('../db/db')();
 
 router.get('/', function(req, res) {
     dbConnection.query('SELECT * FROM customer', (error, rows) => {
-        if(error) res.send(error);
-        res.send(rows);
+        if (error) {
+            res.send(error);
+        }
+        else {
+            res.send(rows);
+        }
     })
 });
 
