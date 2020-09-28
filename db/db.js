b2475a4862b4c70f4003ef461a77e0e213d7fa48
문승훈 <mbh023@gmail.com>
2020-09-28 12:19:19 +0900

    Disconnect Error Handling

--- db/db.js
+++ db/db.js
@@ -1,11 +1,32 @@
 const mysql = require('mysql');
 const settings = require('./settings.js');
 
-const dbConnection = mysql.createConnection(settings);
+// const dbConnection = mysql.createConnection(settings);
 
-dbConnection.connect(function(err){
-    if(err) throw err;
-    console.log("MySQL Database is Connected!");
-});
 
-module.exports = dbConnection;
+//dissconnectiion Handling
+//10~20초 사이마다 새로운 쿼리가 없을 시 disconnect된다.
+
+function handleDisconnect() {
+    const dbConnection = mysql.createConnection(settings);
+    dbConnection.connect(function(err){
+        if (err) {
+            throw err;
+            setTimeout(handleDisconnect, 2000);
+        } else {
+            console.log("MySQL Database is Connected!");
+        }
+    });
+
+    dbConnection.on('error', function(err) {
+        console.error('Database Error :', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+
+module.exports = handleDisconnect();
