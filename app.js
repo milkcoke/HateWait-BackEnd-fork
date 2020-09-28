f5acf18b29e957444f681f9693531c0eb852d024
문승훈 <mbh023@gmail.com>
2020-09-28 12:52:01 +0900

    Disconnect Error Handling

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const customerRouter = require('./routes/customer');
 
 const app = express();
 
-const dbConnection = require('./db/db');
+const dbConnection = require('./db/db')();
 
 
-dbConnection().query('SHOW TABLES', function(err, result){
+dbConnection.query('SHOW TABLES', function(err, result){
   if (err) throw err;
   console.log('result: ', result);
 });
--- routes/customer.js
+++ routes/customer.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const dbConnection = require('../db/db');
+const dbConnection = require('../db/db')();
 
 router.get('/', function(req, res) {
-    dbConnection().query('SELECT * FROM customer', (error, rows) => {
+    dbConnection.query('SELECT * FROM customer', (error, rows) => {
         if(error) res.send(error);
         res.send(rows);
     })
