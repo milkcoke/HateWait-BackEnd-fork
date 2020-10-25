3d86accb8809c96cce42519fa53520e9118b18b2
milkcoke <mbh023@konkuk.ac.kr>
2020-10-25 20:44:25 +0900

    "ADD store id_exist check"

--- routes/waiting_customer.js
+++ routes/waiting_customer.js
@@ -32,26 +32,28 @@ router.get('/:id', (request, response)=> {
                 stack: null
             }
         });
+    } else {
+        const sql = 'SELECT name, people_number FROM waiting_customer WHERE store_id=?';
+        dbConnection().execute(sql, [request.params.id], (error, rows)=> {
+            if (error) {
+                return response.status(500).json({
+                    message: "서버 오류입니다."
+                });
+            } else if(rows.length <= 0) {
+                return response.status(409).json({
+                    message: "아무런 손님이 없어요",
+                    number: 0
+                });
+            } else {
+                return response.status(200).json({
+                    message: "조회 성공!",
+                    waiting_customers: rows
+                })
+            }
+        });
     }
 
-    const sql = 'SELECT name, people_number FROM waiting_customer WHERE store_id=?';
-    dbConnection().execute(sql, [request.params.id], (error, rows)=> {
-        if (error) {
-           return response.status(500).json({
-               message: "서버 오류입니다."
-           });
-        } else if(rows.length <= 0) {
-            return response.status(409).json({
-                message: "아무런 손님이 없어요",
-                number: 0
-            });
-        } else {
-            return response.status(200).json({
-                message: "조회 성공!",
-                waiting_customers: rows
-            })
-        }
-    })
+
 });
 
 module.exports = router;
