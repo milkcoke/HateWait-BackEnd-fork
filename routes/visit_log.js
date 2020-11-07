c2cba5546c413c6199af3f7070a2a4b687cf2172
milkcoke <mbh023@konkuk.ac.kr>
2020-11-07 23:25:34 +0900

    "ADD Inquiry visit_log by storeId and Mode"

--- routes/visit_log.js
+++ routes/visit_log.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const getPoolConnection = require('../db/db2');
 const period = require('../config/visit_log_default_period_settings');
+const check_id = require('../db/check_id');
 
 //only support 'Web' application
 //전체 가게 이용 내역 조회
@@ -13,27 +14,44 @@ router.get('/:storeId/:mode', (request, response)=> {
             message: "올바르지 않은 요청입니다."
         });
     }
-    // 기간 설정 필요한 경우.
-    //추가 : WHERE store_id=? AND DATE(visit_time) BETWEEN (NOW() - INTERVAL ? ?) AND NOW()
-    // 총 손님 수..?
+        check_id.store(request.params.storeId)
+        .catch(error=>{
+            return response.status(500).json({
+                message: "서버 내부 오류입니다."
+            });
+        })
+        .then(storeId=>{
+            if (!storeId) {
+                return response.status(404).json({
+                    message : "헤잇웨잇에 가입된 가게가 아닙니다."
+                });
+            } else {
+                return storeId
+            }
+        })
+        .then(storeId=> {
+            console.log(`storeId (visit_log) : ${storeId}`);
+            // 기간 설정 필요한 경우.
+            //추가 : WHERE store_id=? AND DATE(visit_time) BETWEEN (NOW() - INTERVAL ? ?) AND NOW()
+            // 총 손님 수..?
 
-    //기간 설정 쿼리 예시
-    // SELECT DATE(visit_time) , customer_number
-    // FROM visit_log
-    // WHERE store_id='bani123' AND DATE(visit_time) BETWEEN (NOW() - INTERVAL 7 DAY) AND NOW()
-    // ORDER BY visit_time DESC;
-        let sql = null;
+            //기간 설정 쿼리 예시
+            // SELECT DATE(visit_time) , customer_number
+            // FROM visit_log
+            // WHERE store_id='bani123' AND DATE(visit_time) BETWEEN (NOW() - INTERVAL 7 DAY) AND NOW()
+            // ORDER BY visit_time DESC;
+            let sql = null;
 
-        switch (request.params.mode) {
-            case 'month' :
-                sql = `SELECT DATE_FORMAT(visit_time, '%Y-%m') AS visit_date_month, SUM(customer_number) AS monthly_customer_number
+            switch (request.params.mode) {
+                case 'month' :
+                    sql = `SELECT DATE_FORMAT(visit_time, '%Y-%m') AS visit_date_month, SUM(customer_number) AS monthly_customer_number
                         FROM visit_log
                         WHERE store_id = ?
                         GROUP BY visit_date_month
                         ORDER BY visit_date_month DESC`;
-                break;
-            case 'week' :
-                sql = `SELECT CONCAT(DATE_FORMAT(DATE_SUB(visit_time, INTERVAL WEEKDAY(visit_time)*1 DAY), '%Y-%m-%d'),
+                    break;
+                case 'week' :
+                    sql = `SELECT CONCAT(DATE_FORMAT(DATE_SUB(visit_time, INTERVAL WEEKDAY(visit_time)*1 DAY), '%Y-%m-%d'),
                                 '~',
                                 DATE_FORMAT(DATE_ADD(visit_time, INTERVAL 6 - WEEKDAY(visit_time) DAY), '%Y-%m-%d')) AS visit_date_week,
                                 SUM(customer_number) AS weekly_customer_numeber
@@ -41,36 +59,37 @@ router.get('/:storeId/:mode', (request, response)=> {
                         WHERE store_id = ?
                         GROUP BY visit_date_week
                         ORDER BY visit_date_week DESC`;
-                break;
-            case 'day' :
-                sql = `SELECT DATE(visit_time) AS visit_date, SUM(customer_number) AS daily_customer_number
+                    break;
+                case 'day' :
+                    sql = `SELECT DATE(visit_time) AS visit_date, SUM(customer_number) AS daily_customer_number
                        FROM visit_log
                        WHERE store_id = ?
                        GROUP BY visit_date
                        ORDER BY visit_date DESC`;
-                break;
-            default :
-                break;
-        }
+                    break;
+                default :
+                    break;
+            }
 
-        getPoolConnection(connection=>{
-            connection.execute(sql, [request.params.storeId], (error, rows) => {
-                connection.release();
-                if (error) {
-                    console.error(error);
-                    return response.status(500).json({
-                        message: "서버 내부 오류입니다."
-                    })
-                } else if (rows.length === 0) {
-                    return response.status(200).json({
-                        message: "아직 방문 기록이 없어요!"
-                    });
-                } else {
-                    return response.status(200).json({
-                        rows
-                    })
-                }
-            })
+            getPoolConnection(connection=>{
+                connection.execute(sql, storeId, (error, rows) => {
+                    connection.release();
+                    if (error) {
+                        console.error(error);
+                        return response.status(500).json({
+                            message: "서버 내부 오류입니다."
+                        })
+                    } else if (rows.length === 0) {
+                        return response.status(200).json({
+                            message: "아직 방문 기록이 없어요!"
+                        });
+                    } else {
+                        return response.status(200).json({
+                            rows
+                        })
+                    }
+                });
+            });
         });
 
 });
@@ -79,3 +98,5 @@ router.get('/:storeId/:mode', (request, response)=> {
 // router.get('/:storeId',(request, response)=>{
 //
 // });
+
+module.exports = router;
