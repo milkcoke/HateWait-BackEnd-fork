288ef142df7668ca299925ceac5e78c4e8c428ac
milkcoke <mbh023@konkuk.ac.kr>
2020-11-11 01:17:23 +0900

    "Trying blur out name in server (covering Hangul & English)"

--- function/blur_out_name.js
+++ function/blur_out_name.js
@@ -2,31 +2,35 @@
 
 
 module.exports = async (name)=>{
-    const mosaicCharacter = 'X';
-
+    const mosaicCharacter = 'x';
+    let pendingResult;
     switch(name.length) {
-        //한글이름 2~4자
+
+        //한글이름 2~4자, 물론 4자 이하의 영문이름으로 가입해도 공통적으로 적용됨
         case 2 :
-            return name.charAt(0) + mosaicCharacter;
+            pendingResult = await (name.charAt(0) + mosaicCharacter);
             break;
         case 3 :
-            return name.charAt(0) + mosaicCharacter + name.charAt(2);
+            pendingResult = await (name.charAt(0) + mosaicCharacter + name.charAt(2));
             break;
         case 4 :
-            return name.charAt(0) + "XX" + name.charAt(3);
+            pendingResult = await (name.charAt(0) + "XX" + name.charAt(3));
             break;
         //    그 이상은 영문이름
         default :
             //String.prototype.replaceAll called with a non-global RegExp argument
-            const englishRegex = RegExp('a-zA-Z');
+            // https://developer.mozilla.org/ko/docs/Web/JavaScript/Guide/%EC%A0%95%EA%B7%9C%EC%8B%9D
+            //문제의 원흉은 replaceAll 함수를 인식하지 못한다는것.
             const spaceIndex = name.indexOf(" ");
             //  First name + Last name 일 경우
             if (spaceIndex !== -1) {
-                return name.substring(0, spaceIndex).replaceAll(englishRegex, mosaicCharacter) + name.substring(spaceIndex + 1);
+                pendingResult = await (mosaicCharacter.repeat(spaceIndex) + name.substring(spaceIndex+1));
             } else {
                 const midLength = name.length / 2;
-                return name.substring(0, midLength).replaceAll(englishRegex, mosaicCharacter) + name.substring(midLength + 1);
+                pendingResult = await (mosaicCharacter.repeat(midLength) + name.substring(midLength));
             }
             break;
     }
+
+    return pendingResult;
 }
