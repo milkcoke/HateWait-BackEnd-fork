5dafc7f05622947130e8736110fd411a612401f9
milkcoke <mbh023@konkuk.ac.kr>
2020-11-13 19:19:08 +0900

    "ADD Read Coupon-information// UPDATE db Single Connection => Pooling "

--- routes/coupon_information.js
+++ routes/coupon_information.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({mergeParams : true});
-const getPoolConnection = require('/db/dbConnection.js');
+const getPoolConnection = require('../db/dbConnection');
 
 router.get('/', (request, response)=>{
     const storeId = request.params.id;
