25e30798c9a2ee2479bffc67b19100e89d3703df
문승훈 <mbh023@gmail.com>
2020-11-15 00:29:56 +0900

    "ADD express-ws TESTING ~ 'websocket' communication server-client"

--- ws_routes/waiting_customers.js
+++ ws_routes/waiting_customers.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.ws('/', (ws, request)=>{
+    console.log('we requested from routing!')
     ws.on('message', msg=>{
         ws.send(`server send : ${msg}`);
         console.log(msg, "I'm in ws route!!");
