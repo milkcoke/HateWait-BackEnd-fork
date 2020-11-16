c27cc4f9523dc1f34b6993674e5e2d47ba07ca5a
milkcoke <mbh023@konkuk.ac.kr>
2020-11-16 21:13:49 +0900

    "UPDATE broadcast null check"

--- function/broadcast.js
+++ function/broadcast.js
@@ -2,7 +2,7 @@ const ws = require('ws');
 
 module.exports = function (clients, message) {
     console.log(`clients : ${clients}`);
-    if (clients.length === 0) return;
+    if (!clients.length) return;
     clients.forEach(client=>{
         if(client.readyState === ws.OPEN){
             client.send(message);
