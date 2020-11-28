f4f7bca898671cd97f6638b943d927aab2c2493c
milkcoke <mbh023@konkuk.ac.kr>
2020-11-28 22:32:02 +0900

    "UPDATE typo"

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(express.json());
 //         }
 //     })
 // );
-app.use(flash());
+// app.use(flash());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
