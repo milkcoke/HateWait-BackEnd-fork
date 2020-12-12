9a3a7749b1c7d359cefe5c9adcacb1fcbe4340d9
milkcoke <mbh023@konkuk.ac.kr>
2020-12-12 20:47:56 +0900

    UPDATE typo

--- routes/session_check.js
+++ routes/session_check.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 router.get('/', (request, response)=>{
-    response.json(storeMap);
+    console.log('===storeMap===');
+    console.log(global.storeMap);
+
+    return response.json(global.storeMap);
 });
 
 module.exports = router;
